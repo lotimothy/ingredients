fd228ef0383489ba57c6a660efb69348994d1807
Timothy Lo <tim7643@gmail.com>
2017-01-24 17:25:16 -0800

    updated start page

--- application/views/users_index.php
+++ application/views/users_index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 
-<head> 
+<head>
 	<meta charset="UTF-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Ingredients</title>
@@ -18,25 +18,33 @@
 
 <body ng-app="recipes">
 	<div class="container" ng-controller="ingredientsController">
-		<div ng-view></div>
-		<h4> How many recipes do you have? </h4>
+
+		<h4>How many recipes do you have?</h4>
 		<div class="input-field">
 			<input type="text"id="numRecipes" ng-model="numRecipes">
-			<label for="numRecipes">How many recipes do you have?</label>
+			<label for="numRecipes">(currently works for Food Network recipes)</label>
 		</div>
 		<a ng-click="addUrls()" class="btn waves-effect waves-light center-align">Submit</a>
+
 		<form ng-submit="get()">
-			<div id="siteEntry">
-			</div>
-			<button class="btn waves-effect waves-light center-align" type="submit">Crawl It</button>
+			<div id="siteEntry"></div>
+			<button class="btn waves-effect waves-light center-align right" type="submit">Crawl It</button>
 		</form>
-		<p>http://www.foodnetwork.com/recipes/alton-brown/good-eats-roast-turkey-recipe.html<br><br>
-			http://www.foodnetwork.com/recipes/ina-garten/perfect-roast-turkey-recipe4.html<br><br>
-			http://www.foodnetwork.com/recipes/tyler-florence/mashed-potatoes-recipe.html<br><br>
-			http://www.foodnetwork.com/recipes/giada-de-laurentiis/ciabatta-stuffing-with-chestnuts-and-pancetta-recipe.html<br><br>
+
+		<h5>Samples:</h5>
+		<p>http://www.foodnetwork.com/recipes/ina-garten/perfect-roast-turkey-recipe4.html<br>
+			http://www.foodnetwork.com/recipes/tyler-florence/mashed-potatoes-recipe.html<br>
+			http://www.foodnetwork.com/recipes/giada-de-laurentiis/ciabatta-stuffing-with-chestnuts-and-pancetta-recipe.html<br>
 			http://www.foodnetwork.com/recipes/food-network-kitchens/apple-cobbler-for-two.html
 		</p>
+
+		<h4>Your compiled ingredients:</h4>
+		<div ng-view></div>
+
 	</div>
+
+	<!-- <p><a href="http://simplehtmldom.sourceforge.net/manual.htm" target="_new">simple html dom</a></p> -->
+
 </body>
 
 </html>
